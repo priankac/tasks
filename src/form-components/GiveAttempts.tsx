59c5577f6fcec35b57a2f2f53950385b74a0f1c0
Priyanka Chaudhuri <priankac@udel.edu>
2026-09-20 11:21:06 -0400

    GiveAttempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -5,9 +5,31 @@ export function GiveAttempts(): React.JSX.Element {
     const [attmpts, setAttmpts] = useState<number>(3);
     const [requested, setRequested] = useState<string>("0");
     const prse: number = parseInt(requested, 10);
+    function handleUse(): void {
+        setAttmpts(attmpts - 1);
+    }
+    function handleGains(): void {
+        if (!Number.isNaN(prse)) {
+            setAttmpts(attmpts + prse);
+        }
+    }
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Form.Group controlId="requestedAttempts">
+                <Form.Label>Requested Attempts:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requested}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setRequested(event.target.value);
+                    }}
+                />
+            </Form.Group>
+            <Button disabled={attmpts === 0} onClick={handleUse}>
+                use
+            </Button>
+            <Button onClick={handleGains}>gain with each use</Button>
         </div>
     );
 }
