41e171b70d261d30ca35c12272e9806c4290c8a3
Priyanka Chaudhuri <priankac@udel.edu>
2026-09-20 11:12:37 -0400

    imports

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attmpts, setAttmpts] = useState<number>(3);
+    const [requested, setRequested] = useState<string>("0");
+    const prse: number = parseInt(requested, 10);
     return (
         <div>
             <h3>Give Attempts</h3>
