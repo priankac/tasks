fe48d25b0319adb906faaccfea2e7a97320bb853
Priyanka Chaudhuri <priankac@udel.edu>
2026-09-20 10:33:01 -0400

    import

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [ans, setAns] = useState<string>("");
     return (
         <div>
             <h3>Check Answer</h3>
