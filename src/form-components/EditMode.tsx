0863907aa92a3442e0294654e46416a4273097d7
Priyanka Chaudhuri <priankac@udel.edu>
2026-09-20 11:33:15 -0400

    const

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [makeEdits, setEdits] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [student, setStudent] = useState<boolean>(true);
     return (
         <div>
             <h3>Edit Mode</h3>
