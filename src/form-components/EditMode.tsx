eec0372133c833b5de7abfd9f39ba7caf5ec4571
Priyanka Chaudhuri <priankac@udel.edu>
2026-09-20 11:36:21 -0400

    made functions

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -5,6 +5,16 @@ export function EditMode(): React.JSX.Element {
     const [makeEdits, setEdits] = useState<boolean>(false);
     const [name, setName] = useState<string>("Your Name");
     const [student, setStudent] = useState<boolean>(true);
+
+    function createEdit(event: React.ChangeEvent<HTMLInputElement>): void {
+        setEdits(event.target.checked);
+    }
+    function changeName(event: React.ChangeEvent<HTMLInputElement>): void {
+        setName(event.target.value);
+    }
+    function checkStudent(event: React.ChangeEvent<HTMLInputElement>): void {
+        setStudent(event.target.checked);
+    }
     return (
         <div>
             <h3>Edit Mode</h3>
